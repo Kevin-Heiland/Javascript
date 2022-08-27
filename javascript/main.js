bf153c6f49e5297093073319e289f8a1141626a4
Kevin Heiland <kevin_heiland@hotmail.com>
2022-08-27 20:43:28 -0300

    Ciclos

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+
+
+let ingresarNumero = Number(prompt('ingrese un numero'));
+let i = Number(prompt('ingrese un numero por el cual desea multiplicar'))
+
+for (let i = 1; i <= i; i++) {
+    let resultado = ingresarNumero * i;
+    alert (ingresarNumero+ 'x'+i+ '='+resultado);
+}
