eacc9e868e202ef36e9c0e6013e33b44a8787eea
Kevin Heiland <kevin_heiland@hotmail.com>
2022-09-04 21:52:49 -0300

    simulador

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,24 @@
 
 
-let ingresarNumero = Number(prompt('ingrese un numero'));
-let i = Number(prompt('ingrese un numero por el cual desea multiplicar'))
+// let ingresarNumero = Number(prompt('ingrese un numero'));
+// let i = Number(prompt('ingrese un numero por el cual desea multiplicar'))
 
-for (let i = 1; i <= i; i++) {
-    let resultado = ingresarNumero * i;
-    alert (ingresarNumero+ 'x'+i+ '='+resultado);
+// for (let i = 1; i <= i; i++) {
+//     let resultado = ingresarNumero * i;
+//     alert (ingresarNumero+ 'x'+i+ '='+resultado);
+// }
+
+function solicitarPrecio() {
+
+    let valorIngresado = parseInt(prompt("Ingrese el precio del producto para saber el precio final:"));
+    const ivaProducto = valorIngresado + (valorIngresado * 0.21);
+    alert("El precio del producto mas el IVA es: " + ivaProducto);
+
 }
+
+let seguirCotizando;
+
+do {
+    solicitarPrecio();
+    seguirCotizando = confirm("Desea saber otro precio?");
+} while (seguirCotizando);
