f204f1eac5fa21720e99829d8f67770f000a445b
Kevin Heiland <kevin_heiland@hotmail.com>
2022-09-13 10:46:24 -0300

    array

---
+++ javascript/stock.js
@@ -0,0 +1,28 @@
+
+const productos = [
+    {
+        id: 1,
+        nombre: "Lemon pie",
+        descripcion: "El postre mas rico, si te gusta el limon. con limones seleccionados y materia prima de excelente calidad",
+        precio: 2500,
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "cañonsito",
+        descripcion: "Hechos de hojaldre y rellenos de dulce de leche",
+        precio: 600,
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "chipa",
+        descripcion: "Con harina de mandioca y queso parmesano de la mejor calidad.",
+        precio: 800,
+        cantidad:1
+    },
+
+    
+]
