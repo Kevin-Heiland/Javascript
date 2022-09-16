d21397ccbc1644836c55cab33695b2d53c74c09e
Kevin Heiland <kevin_heiland@hotmail.com>
2022-09-15 22:08:11 -0300

    primer entrega

--- javascript/stock.js
+++ javascript/stock.js
@@ -24,19 +24,136 @@
 //         cantidad:1
 //     },
 
-    
-// ]
 
-const productos = ['chipa', 'palmeritas', 'lemon pie']
-
-let elijaProd = '';
-
-do {
-    if ( elijaProd != '' ) {
-        alert ('El producto a sido seleccionado');
+class alimentos {
+    constructor (nombre, precio, stock) {
+        this.nombre= nombre;
+        this.precio= precio;
+        this.stock= stock;
     }
 
-    elijaProd = prompt ('Ingrese el producto que sea comprar! (chipa, palmeritas o lemon pie)')
-} while (productos.includes (elijaProd));
+    actualizarStock(X){
+        this.stock= this.stock - X;
+    }
+  
+}
 
-alert ('El producto '+elijaProd+' no se encuentra disponible')
+const arrayAlimentos = [];
+arrayAlimentos.push(new alimentos ("chipa", 100, 20));
+arrayAlimentos.push(new alimentos ("palmeritas", 300, 20));
+arrayAlimentos.push(new alimentos ("lemon pie", 600, 15));
+
+//Precio Menor a Mayor
+const ordenarMenorMayor = () => {
+    arrayAlimentos.sort((a,b)=> a.precio - b.precio);
+    console.log(arrayAlimentos);
+    mostrarListaOrdenada();
+}
+
+//Precio Mayor a Menor
+const ordenarMayorMenor = () => {
+    arrayAlimentos.sort((a,b)=> b.precio - a.precio);
+    console.log(arrayAlimentos);
+    mostrarListaOrdenada();
+}
+
+const mostrarListaOrdenada = () => {
+    let array = [];
+    arrayAlimentos.forEach(alimentos => array.push(alimentos.nombre+" $"+alimentos.precio));
+    alert("Lista de precios:"+"\n"+array.join("\n"))
+}
+
+
+// FUNCTIONS PARA PROCESO DE COMPRA
+
+let total= 0;
+
+const agregarProductoAlCarrito = () => {
+    let productoAgregado;
+    let producto = "";
+    let cantidad = 0;
+    let precio = 0;
+
+    //Ciclo para sumar productos al carrito
+    do {
+        producto = prompt ("¿Que alimentos desea comprar. tenemos para ofrecerle (chipa, palmeritas, lemon pie."); 
+        cantidad = parseInt(prompt ("¿Cuántos desea comprar?"));
+        console.log(cantidad)
+
+        switch (producto) {
+            case arrayAlimentos[0].nombre:
+                arrayAlimentos[0].actualizarStock(cantidad);
+                if (arrayAlimentos[0].stock <= 0 || isNaN(cantidad)){
+                    alert("Lo sentimos. En este momento no tenemos stock")
+                    arrayAlimentos[0].stock=arrayAlimentos[0].stock+cantidad;
+                     precio = 0;
+                    cantidad = 0;
+                }else{
+                    precio= arrayAlimentos[0].precio;
+                }
+                break;
+            case arrayAlimentos[1].nombre:
+                arrayAlimentos[1].actualizarStock(cantidad);
+                if (arrayAlimentos[1].stock<=0 || isNaN(cantidad)){
+                    alert("Lo sentimos. En este momento no tenemos stock")
+                    arrayAlimentos[1].stock=arrayAlimentos[1].stock+cantidad;
+                    precio = 0;
+                    cantidad = 0;
+                }else{
+                    precio= arrayAlimentos[1].precio;
+                }
+                break;
+            case arrayAlimentos[2].nombre:
+                arrayAlimentos[2].actualizarStock(cantidad);
+                if (arrayAlimentos[2].stock<=0 || isNaN(cantidad)){
+                    alert("Lo sentimos. En este momento no tenemos stock")
+                    arrayAlimentos[2].stock=arrayAlimentos[2].stock+cantidad;
+                    precio = 0;
+                    cantidad = 0;
+                }else{
+                    precio= arrayAlimentos[2].precio;
+                }
+                break;
+            default:
+                alert("Alguno de los ingresados no es correcto");
+                precio= 0;
+                cantidad= 0;
+        }
+        total = total + precio*cantidad;
+        productoAgregado = confirm("¿Desea agregar otro producto?");
+    } while (productoAgregado);
+}
+
+//Función para calcular el envío
+const obtenerPrecioDeEnvio = (total) => {
+    let confirmacion = confirm("¿Querés envio a domicilio?");
+    if (confirmacion && total>=1000){
+        alert("Tenés envío gratis. El total de tu compra es $"+total);
+    }else if (confirmacion && total<1000 && total!=0){
+        total=total+700;
+        alert("El envío cuesta $700. El total de tu compra asciende a $"+total);
+    }else{
+        alert("El total de tu compra es $"+total);
+    }
+    return total;
+}
+
+//Calculo el total de carrito
+const obtenerTotal = (total) => {
+    alert ("El total a pagar es $"+total);
+}
+
+
+// llamando funciones
+
+comprarProductos = () => {
+    if (confirm("¿Querés ordenar la lista de alimentos del más barato al más caro?")){
+        ordenarMenorMayor();
+    }else{
+        ordenarMayorMenor();
+    }
+    agregarProductoAlCarrito();
+    obtenerTotal(obtenerPrecioDeEnvio(total));
+}
+
+comprarProductos();
